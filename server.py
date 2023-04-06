2c203df5246bdf07c3d5a7bfc0ddf874a71b8857
Thien1Nguyen <tnguyen5219@gmail.com>
2023-04-06 16:21:26 -0700

    Ninja Bonus

--- server.py
+++ server.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/play/<int:int>/<color>')
-def box(int, color):
+@app.route('/play')
+def fix_box(int= 3, color ="cornflowerblue"):
+    return render_template("index.html", int=int, color= color)
+
+@app.route('/play/<int:int>')
+def int_box(int, color ="cornflowerblue"):
+    return render_template("index.html", int=int, color= color)
+
+@app.route('/play/<int:int>/<string:color>')
+def box(int, color ="cornflowerblue"):
     return render_template("index.html", int=int, color= color)
 
 if __name__ =="__main__":
